fe67c7a8314b98ebd6ef8a5c513ea41d41e2b6c9
Brian Ibbotson <operations@quinceleaf.com>
2020-09-28 23:42:22 -0400

    Updated api to reflect new routes

---
+++ frontend/api/api.js
@@ -0,0 +1,7 @@
+const baseUrl = process.env.API_BASE_URL;
+
+export const apiEventData = `${baseUrl}/events/`;
+
+export const apiTagData = `${baseUrl}/tags/`;
+
+export const apiOrgData = `${baseUrl}/organizations/`;
