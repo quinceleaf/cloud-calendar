3dd3a208057cf4b4c1300c2086ac1c11e7e84996
Brian Ibbotson <operations@quinceleaf.com>
2020-09-29 22:33:18 -0400

    Increased top margins for Loading and Updating indicators

--- frontend/components/common/Loading.js
+++ frontend/components/common/Loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loading = () => {
-  return <div className="text-blue-500">Loading...</div>;
+  return <div className="mt-3 text-blue-500">Loading...</div>;
 };
 
 export default Loading;
--- frontend/components/common/Updating.js
+++ frontend/components/common/Updating.js
@@ -4,11 +4,11 @@ import { MdAutorenew } from "react-icons/md";
 
 const Updating = () => {
   return (
-    <div className="flex flex-row text-blue-200 text-sm">
+    <div className="flex flex-row text-blue-200 text-2xl">
       <div className="mt-1">
         <MdAutorenew />
       </div>
-      <div>Updating</div>
+      {/* <div>Updating</div> */}
     </div>
   );
 };
